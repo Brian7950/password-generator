6eedba25bfe046e97c4f609ae05507b889978589
Brian7950 <brianh7950@gmail.com>
2022-02-05 12:02:57 -0700

    added prompt and confirm to criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,26 @@
 // Assignment code here
 
+// password length
+var passwordLength = window.prompt("Length of password?");
+// if too short or too long ask again. 
+if(passwordLength < 8 || passwordLength > 128){
+  window.alert("Password must be betwee 8 - 128 characters long");
+  var passwordLength = window.prompt("Length of password?");
+}
+
+//if length is correct continue with criteria 
+while (passwordLength > 8 || passwordLength < 128 ){
+
+var addLowerCase = window.confirm("Do you want to add lower case letters?");
+
+var addUpperCase = window.confirm("Do you want to add UPPER case letters?");
+
+var addNumbers = window.confirm("Do you want to inclue numbers?");
+break;
+}
+
+console.log(passwordLength, addLowerCase, addUpperCase, addNumbers);
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
