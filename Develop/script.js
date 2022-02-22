91e5843fb3de7b84d9c79fe991505db6ed59b68e
Brian7950 <brianh7950@gmail.com>
2022-02-21 20:53:20 -0700

    working pw length and randome pw

--- Develop/script.js
+++ Develop/script.js
@@ -1,57 +1,45 @@
 /*// Assignment code here*/
 
-var upperCaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var lowerCaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-var numberArray = ['0','1','2','3','4','5','6','7','8','9'];
-var charArray = ['!', '@', '#', '$', '%', '&', '*', '(', ')' ,'^'];
-
-console.log(upperCaseArray, lowerCaseArray, numberArray, charArray);
-var emptyPassword = [];
+var upperCaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowerCaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var numberArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var charArray = ['!', '@', '#', '$', '%', '&', '*', '(', ')', '^'];
+var possibleCharacters = [];
 
 //function for shuffling array
-var shuffle = function(array){
-  for(var i = array.length - 1; i > 0; i--){
-      var j = Math.floor(Math.random() * (i + 1));
+var shuffle = function (array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
 
-      var temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
   }
   return array;
 }
 
-//creating final array 
-/*var finalPW = function(array){
-  for(var i = 0; i < array.length && array.length < passwordLength; i++ ){
-    var temp = [i];
-    
-  }
-  return temp;
-}*/
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
- 
-var generatePassword = function() {
+
+var generatePassword = function () {
 
   var passwordLength = window.prompt("Choose length of password");
   console.log("logging password length " + passwordLength);
 
   // if length is anything other than number
-  if(isNaN(passwordLength)) {
+  if (isNaN(passwordLength)) {
     window.alert("Please enter a valid length between 8-128");
     return generatePassword();
   }
   //if length is not between 8-128
-  else if(passwordLength < 8 || passwordLength > 128){
+  else if (passwordLength < 8 || passwordLength > 128) {
     window.alert("Invalid length please enter a number between 8-128");
     return generatePassword();
   }
   console.log(passwordLength);
 
 
-//criteria to add to password 
+  //criteria to add to password 
   var criteriaUpper = window.confirm("Do you want to add Capital letters?");
   console.log("criteriaUpper: " + criteriaUpper);
 
@@ -65,47 +53,37 @@ var generatePassword = function() {
   console.log("criteriaSpecialCharacters: " + criteriaSpecialCharacters);
 
   //if no criteria is chosen (all false)
-  if(!criteriaUpper && !criteriaLower && !criteriaNumber && !criteriaSpecialCharacters) {
+  if (!criteriaUpper && !criteriaLower && !criteriaNumber && !criteriaSpecialCharacters) {
     window.alert("You need to pick at least 1 of the options");
     return generatePassword();
   };
 
-  //generate password based on criteria
-  if(criteriaUpper){
-    emptyPassword.push(...upperCaseArray);
-    console.log(emptyPassword);
-  }
-  if(criteriaLower){
-    emptyPassword.push(...lowerCaseArray);
-    console.log(emptyPassword);
-  }
-  if(criteriaNumber){
-    emptyPassword.push(...numberArray);
-    console.log(emptyPassword)
-  }
-  if(criteriaSpecialCharacters){
-    emptyPassword.push(...charArray);
-    console.log(emptyPassword);
+  for (var i = 0; i < passwordLength; i++) {
+    if (criteriaUpper && possibleCharacters.length < passwordLength) {
+      var rand = upperCaseArray[Math.floor(Math.random() * upperCaseArray.length)];
+      possibleCharacters.push(rand);
+    }
+    if (criteriaLower && possibleCharacters.length < passwordLength) {
+      var rand = lowerCaseArray[Math.floor(Math.random() * lowerCaseArray.length)];
+      possibleCharacters.push(rand);
+    }
+    if (criteriaNumber && possibleCharacters.length < passwordLength) {
+      var rand = numberArray[Math.floor(Math.random() * numberArray.length)];
+      possibleCharacters.push(rand);
+    }
+    if (criteriaSpecialCharacters && possibleCharacters.length < passwordLength) {
+      var rand = charArray[Math.floor(Math.random() * charArray.length)];
+      possibleCharacters.push(rand);
+    }
   }
 
-  //shuffle the selected criteria 
-  shuffle(emptyPassword);
-  console.log(emptyPassword);
+  //shuffle order so it doens't have a pattern (cap,lower,num,symbol)
+  shuffle(possibleCharacters);
 
-  //creating final array according to length 
-var finalPW = function(array){
-  for(var i = 0; i < array.length && array.length < passwordLength; i++ ){
-    var temp = [i];
-    
-  }
-  //return temp;
-}
+  console.log(possibleCharacters);
 
-finalPW(emptyPassword);
-console.log(emptyPassword);
-
-//display password on page and remove "," 
-  return emptyPassword.join("");
+  //display password on page and remove "," 
+  return possibleCharacters.join("");
 
 }
 
